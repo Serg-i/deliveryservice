a35fa217194e2e26fa954d7ae36884ad742559bc
Dmitry <Kosmich27@mail.ru>
2014-04-03 11:04:30 +0300

    Added angular project structure, added orderController and orderService

---
+++ frontend/src/main/webapp/js/controllers/orderController.js
@@ -0,0 +1,60 @@
+var app = angular.module('orderControllers', []);
+
+app.controller('OrdersCtrl', ['$scope', 'OrderREST', '$location',
+    function ($scope, OrderREST, $location) {
+
+        // callback for ng-click 'editOrder':
+        $scope.editOrder = function (orderId) {
+            $scope.order = OrderREST.readOne({ id: orderId });
+            $location.path('/api/edit_order/');
+        };
+
+        // callback for ng-click 'deleteOrder':
+        $scope.deleteOrder = function (checkedOrders) {
+            for (var orderId in checkedOrders) {
+                OrderREST.delete({ id: orderId });
+            }
+            $scope.orders = OrderREST.readAll();
+        };
+
+        // callback for ng-click 'createOreder':
+        $scope.createOrder = function () {
+            $location.path('/api/new_order');
+        };
+
+        // callback for ng-click 'searchOrder':
+        $scope.searchOrder = function () {
+            $location.path('/api/search_order');
+        };
+        $scope.orders = OrderREST.readAll();
+    }]);
+
+app.controller('NewOrderCtrl', ['$scope', 'OrderREST', '$location',
+    function ($scope, OrderREST, $location) {
+
+        // callback for ng-click 'saveOrder':
+        $scope.saveOrder = function () {
+            OrderREST.create();
+            $location.path('/api/orders');
+        };
+    }]);
+
+app.controller('EditOrderCtrl', ['$scope', 'OrderREST', '$location',
+    function ($scope, OrderREST, $location) {
+
+        // callback for ng-click 'saveOrder':
+        $scope.saveOrder = function () {
+            OrderREST.update({ id: $scope.order.id });
+            $location.path('/api/orders');
+        };
+    }]);
+
+app.controller('SearchOrderCtrl', ['$scope', 'OrderREST', '$location',
+    function ($scope, OrderREST, $location) {
+
+        // callback for ng-click 'searchOrder':
+        $scope.searchOrder = function () {
+            //TODO  $scope.orders = OrderREST.search();
+            $location.path('/api/orders');
+        };
+    }]);
---
+++ frontend/src/main/webapp/js/services/orderService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Services */
+
+angular.module("orderService", ["ngResource"]).
+    factory("OrderREST", function($resource) {
+        return $resource("/api/orders/:id", {}, {
+            readAll: {method: "GET", params: {id: ''}, isArray: true},
+            readOne: {method: 'GET', params: {id: '@id'}},
+            create: {method:'POST', params: {id: ''}},
+            update: {method:'PUT', params: {id: '@id'}},
+            delete: {method:'DELETE', params: {id: '@id'}}
+        });
+    });
