fee2208d5c3c0514d8fd16a7afedd9ed947c5526
BlazWin <BlazWin@gmail.com>
2014-04-15 17:18:09 +0300

    Added order-view page.

--- frontend/src/main/webapp/js/services/orderService.js
+++ frontend/src/main/webapp/js/services/orderService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.factory("OrderREST", function($resource) {
-        return $resource("/backend/api/orders/:id", {}, {
+        return $resource("/backend/api/orders/:id:rpath", {}, {
             readOne: {
                 method: 'GET',
                 params: {id: '@id'},
@@ -33,6 +33,20 @@ app.factory("OrderREST", function($resource) {
         });
     });
 
+
+app.factory("OrderStateREST", function($resource) {
+        return $resource("/backend/api/orders/:id/state", {}, {
+            updateState: {
+            	method:'PUT',
+                params: {id: '@id'},
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+       });
+    });
+
+   
 app.factory("OrdersREST", function($resource) {
     return $resource("/backend/api/orders/p/:page", {}, {
         readAll: {
@@ -43,4 +57,53 @@ app.factory("OrdersREST", function($resource) {
             }
         }
     });
+});
+
+app.factory("OrderState", function(ORDER_STATE, LOCAL_STATE) {
+    return {
+        possible: function (state) {
+            var states = new Array();
+            var ind = 0;
+            if (state == ORDER_STATE.canceled || state == ORDER_STATE.closed)
+                return states;
+            if (state == ORDER_STATE.bad) {
+                states[ind++] = {value: ORDER_STATE.new, display: LOCAL_STATE.new};
+                return states;
+            }
+            if (state == ORDER_STATE.new)
+                states[ind++] = {value : ORDER_STATE.accepted, display : LOCAL_STATE.accepted};
+            if (state == ORDER_STATE.accepted)
+                states[ind++] = {value : ORDER_STATE.processing, display : LOCAL_STATE.processing};
+            if (state == ORDER_STATE.processing)
+                states[ind++] = {value : ORDER_STATE.ready, display : LOCAL_STATE.ready};
+            if (state == ORDER_STATE.ready)
+                states[ind++] = {value : ORDER_STATE.delivery, display : LOCAL_STATE.delivery};
+            if (state == ORDER_STATE.delivery)
+                states[ind++] = {value : ORDER_STATE.closed, display : LOCAL_STATE.closed};
+
+            states[ind++] = {value: ORDER_STATE.bad, display: LOCAL_STATE.bad};
+            return states;
+        },
+        getlocal: function (state) {
+            switch (state) {
+                case ORDER_STATE.new:
+                    return LOCAL_STATE.new; break;
+                case ORDER_STATE.accepted:
+                    return LOCAL_STATE.accepted; break;
+                case ORDER_STATE.processing:
+                    return LOCAL_STATE.processing; break;
+                case ORDER_STATE.ready:
+                    return LOCAL_STATE.ready; break;
+                case ORDER_STATE.delivery:
+                    return LOCAL_STATE.delivery; break;
+                case ORDER_STATE.bad:
+                    return LOCAL_STATE.bad; break;
+                case ORDER_STATE.canceled:
+                    return LOCAL_STATE.canceled; break;
+                case ORDER_STATE.closed:
+                    return LOCAL_STATE.closed; break;
+            }
+            return null;
+        }
+    };
 });
