f3f9b796fad7d789c75e3a000b67b59d0dacb5d9
DmitryKosmich <kosmich27@mail.ru>
2014-04-16 23:35:47 +0300

    Added translate for userRoles.

--- frontend/src/main/webapp/js/controllers/applicationController.js
+++ frontend/src/main/webapp/js/controllers/applicationController.js
@@ -8,10 +8,30 @@ app.controller('appCtrl', ['$rootScope', 'USER_ROLES', 'AuthService','Session','
             role: ''
         };
 
+        var convertToRus = function(role) {
+            switch (role){
+                case USER_ROLES.admin:
+                    return "Администратор";
+                case USER_ROLES.supervisor:
+                    return "Супервизор";
+                case USER_ROLES.order_manager:
+                    return "Менеджер по приему заказов";
+                case USER_ROLES.processing_manager:
+                    return "Менеджер по обработке заказов";
+                case USER_ROLES.courier:
+                    return "Курьер";
+                case USER_ROLES.guest:
+                    return "Гость";
+                default:
+                    return "";
+            }
+            return "";
+        }
+
         var initGuest = function() {
             Session.destroy();
             $scope.currentUser.name = Session.username;
-            $scope.currentUser.role = Session.userRole;
+            $scope.currentUser.role = convertToRus(Session.userRole);
             $scope.currentUser.isAuthorized = Session.isAuthorized;
 
             BasicAuth.setCredentials('','');
@@ -21,7 +41,7 @@ app.controller('appCtrl', ['$rootScope', 'USER_ROLES', 'AuthService','Session','
 
         $scope.$on( 'newLogin', function() {
             $scope.currentUser.name = Session.username;
-            $scope.currentUser.role = Session.userRole;
+            $scope.currentUser.role = convertToRus(Session.userRole);
             $scope.currentUser.isAuthorized = Session.isAuthorized;
         } );
 
